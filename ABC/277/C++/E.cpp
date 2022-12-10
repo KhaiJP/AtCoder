a1e600e3b855d93e35084fb08de768a845124b31
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2022-12-10 19:40:56 +0900

    Update E.cpp

--- ABC/277/C++/E.cpp
+++ ABC/277/C++/E.cpp
@@ -1,55 +1,47 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-#define rep(i,n) for(int i=1; i<=n; ++i)
+#define rep(i,n) for(int i=0; i<(n); ++i)
 
 using namespace std;
-typedef pair<int, int> edge;
-
-const int inf = 1e9;
 
 int main(){
-    int N, M, K;
-    cin >> N >> M >> K;
-    vector<edge> E[2*N+2];
-    
-    int u, v, a, s;
-    rep(i, M){
-        cin >> u >> v >> a;
-        if(a){
-            E[u].push_back(edge(v, 1));
-            E[v].push_back(edge(u, 1));
-        }else{
-            E[N+v].push_back(edge(N+u, 1));
-            E[N+u].push_back(edge(N+v, 1));
+    int N, M, K; cin >> N >> M >> K;
+    vector<vector<pair<int,int>>> E(2*N);
+    rep(i,M){
+        int u, v, a; cin >> u >> v >> a;
+        u -= 1; v -= 1;
+        if(a==0){
+            u += N;
+            v += N;
         }
+        E[u].push_back(pair<int,int>(v, 1));
+        E[v].push_back(pair<int,int>(u, 1));
     }
     rep(i,K){
-        cin >> s;
-        E[s].push_back(edge(N+s, 0));
-        E[N+s].push_back(edge(s, 0));
+        int s; cin >> s;
+        s -= 1;
+        E[s].push_back(pair<int,int>(s+N, 0));
+        E[s+N].push_back(pair<int,int>(s, 0));
     }
 
+    // 01-BFS
     deque<int> q;
-    q.push_back(1);
-    int dist[2*N+2];
-    rep(i, 2*N) dist[i] = inf;
-    dist[1] = 0;
-
+    q.push_back(0);
+    vector<int> D(2*N, 1e9);
+    D[0] = 0;
     while(q.size()){
-        int v = q.front(); q.pop_front();
-        rep(i, (int)E[v].size()){
-            int u = E[v][i-1].first, d_vu = E[v][i-1].second;
-            if(dist[u] > dist[v]+d_vu){
-                dist[u] = dist[v] + d_vu;
-                if(d_vu == 0) q.push_front(u);
-                else q.push_back(u);
+        int u = q.front(); q.pop_front();
+        rep(i, (int)E[u].size()){
+            int v = E[u][i].first; int d = E[u][i].second;
+            if(D[v] > D[u] + d){
+                D[v] = D[u] + d;
+                if(d) q.push_back(v);
+                else  q.push_front(v);
             }
         }
     }
-
-    int ans = min(dist[N], dist[2*N]);
-    if(ans == inf) ans = -1;
-    cout << ans << endl;
+    int answer = min(D[N-1], D[2*N-1]);
+    cout << (answer == 1e9 ? -1 : answer) << endl;
     return 0;
 }
