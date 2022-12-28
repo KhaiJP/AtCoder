81c0aa81c9494b79b57205f1d40016d35a867d9f
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2022-12-28 23:24:28 +0900

    Solved 233D

--- ABC/<300/233/C++/D.cpp
+++ ABC/<300/233/C++/D.cpp
@@ -3,17 +3,21 @@
 #define rep(i,t) for(int i=0;i<(t);++i)
 using namespace std;
 
+/*
+  Consider the prefix sum of A, if pfx[n]-K happens m times in n in 0:n-1,
+  then m patterns of subseries that of K are possible.
+*/
 int main(){
-  int64_t n, k; cin >> n >> k;
-  unordered_map<int64_t, int64_t> p;
+  int64_t N, K; cin >> N >> K;
+  unordered_map<int64_t, int64_t> history;
   int64_t pfx = 0;
-  int64_t ans = 0;
-  rep(i,n){
-    p[pfx] += 1;
-    int64_t a; cin >> a;
-    pfx += a;
-    ans += p[pfx-k];
+  int64_t answer = 0;
+  rep(i, N){
+    history[pfx] += 1;
+    int64_t A; cin >> A;
+    pfx += A;
+    answer += history[pfx-K];
   }
-  cout << ans << endl;
+  cout << answer << endl;
   return 0;
 }
