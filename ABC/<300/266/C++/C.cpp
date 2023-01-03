8e8c56f7d68807664f040a3b9571dc83157e597b
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-01-03 11:55:29 +0900

    Solved 266C

--- ABC/<300/266/C++/C.cpp
+++ ABC/<300/266/C++/C.cpp
@@ -14,7 +14,7 @@ int main(){
 }
 
 bool isOtherSide_wrt_l1_l2(P l1, P l2, P p1, P p2){
-  // line l1_l2
+  // check if p1 and p2 are on the other side with respect to line l1_l2. line l1_l2 is given below;
   // (l2.first - l1.first)*(y - l1.second) - (l2.second - l1.second)*(x - l1.first)
   bool sgn_p1 = (l2.first - l1.first)*(p1.second - l1.second) - (l2.second - l1.second)*(p1.first - l1.first) > 0;
   bool sgn_p2 = (l2.first - l1.first)*(p2.second - l1.second) - (l2.second - l1.second)*(p2.first - l1.first) > 0;
