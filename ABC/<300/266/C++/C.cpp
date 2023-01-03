963062241a9bd6b3c1101408e38ec658a4f05788
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-01-03 12:03:41 +0900

    Update C.cpp

--- ABC/<300/266/C++/C.cpp
+++ ABC/<300/266/C++/C.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 typedef pair<int, int> P;
 
-bool isOtherSide_wrt_l1_l2(P l1, P l2, P p1, P p2);
+bool onOtherSide_wrt_q1_q2(P q1, P q2, P p1, P p2);
+
 int main(){
   P A; cin >> A.first >> A.second;
   P B; cin >> B.first >> B.second;
   P C; cin >> C.first >> C.second;
   P D; cin >> D.first >> D.second; 
-  bool isConvex = isOtherSide_wrt_l1_l2(A,C,B,D) && isOtherSide_wrt_l1_l2(B,D,A,C);
+  // if B and D are on the other side with respect to line AC,
+  // and A and C are on the other side with respect to line BD, then ABCD is convex.
+  bool isConvex = onOtherSide_wrt_q1_q2(A,C,B,D) && onOtherSide_wrt_q1_q2(B,D,A,C);
   cout << (isConvex ? "Yes" : "No") << endl;
   return 0;
 }
 
-bool isOtherSide_wrt_l1_l2(P l1, P l2, P p1, P p2){
-  // check if p1 and p2 are on the other side with respect to line l1_l2. line l1_l2 is given below;
-  // (l2.first - l1.first)*(y - l1.second) - (l2.second - l1.second)*(x - l1.first)
-  bool sgn_p1 = (l2.first - l1.first)*(p1.second - l1.second) - (l2.second - l1.second)*(p1.first - l1.first) > 0;
-  bool sgn_p2 = (l2.first - l1.first)*(p2.second - l1.second) - (l2.second - l1.second)*(p2.first - l1.first) > 0;
+bool onOtherSide_wrt_q1_q2(P q1, P q2, P p1, P p2){
+  // check if p1 and p2 are on the other side with respect to line q1_q2. line q1_q2 is given below;
+  // (q2.first - q1.first)*(y - q1.second) - (q2.second - q1.second)*(x - q1.first)
+  bool sgn_p1 = (q2.first - q1.first)*(p1.second - q1.second) - (q2.second - q1.second)*(p1.first - q1.first) > 0;
+  bool sgn_p2 = (q2.first - q1.first)*(p2.second - q1.second) - (q2.second - q1.second)*(p2.first - q1.first) > 0;
+  // to be on the other side, the signs musd be different;
   return sgn_p1 != sgn_p2;
 }
