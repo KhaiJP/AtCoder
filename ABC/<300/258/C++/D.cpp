357d2533c0b482730f07c724fc2c7e5d6a042edc
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-01-06 18:29:22 +0900

    Update D.cpp

--- ABC/<300/258/C++/D.cpp
+++ ABC/<300/258/C++/D.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main(){
   int64_t N, X; cin >> N >> X;
-  vector<int64_t> A(N), B(N); rep(i,0,N) cin >> A[i] >> B[i];
-  int64_t answer = 1e20;
-  int64_t ref = 0;
-  rep(i,0,N){
-    if(i == X) break;
-    int64_t tmp = ref + A[i] + (X-i)*B[i];
-    answer = tmp < answer ? tmp : answer;
-    ref += A[i] + B[i];
+  vector<int64_t> A(N+1,0), B(N+1,0); rep(i,1,N+1) cin >> A[i] >> B[i];
+  int64_t answer = 1e20, elapsed_time = 0;
+  rep(i,1,N+1){
+    int64_t play_time = elapsed_time + A[i] + X*B[i];
+    answer = play_time < answer ? tmp : answer;
+    elapsed_time += A[i] + B[i];
+    X -= 1;
+    if(X==0) break;
   }
   cout << answer << endl;
   return 0;
