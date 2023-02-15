3ca5ea9164b7ab3d1565c4035962c2e2455e042f
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-02-16 07:58:26 +0900

    Solved EDPC_I

---
+++ Educational_DP_Contest/C++/I.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+#define rep(i, s, t) for(int i = (s); i < (t); ++i)
+#define N_MAX 3010
+
+// dp[ i ][ j ] : probability that j coins, up to 1 to i, show heads 
+double dp[N_MAX][N_MAX];
+
+int main(){
+  int N; cin >> N;
+
+  // 0 coin show head up to coin 0 is 1, or just for convenience 
+  dp[0][0] = 1;
+  rep(i, 0, N){
+    double p; cin >> p;
+    rep(j, 0, i+1){
+      // when coin i+1 show head
+      dp[ i + 1 ][ j + 1 ] += dp[ i ][ j ] * p;
+      // when coin i+1 show tail
+      dp[ i + 1 ][ j ] += dp[ i ][ j ] * (1-p); 
+    }
+  }
+  
+  double answer = 0;
+  // more heads than tails <==> more than N/2 coins show heads
+  rep(j, N/2+1, N+1) answer += dp[ N ][ j ];
+
+  cout << fixed << setprecision(10) << answer << endl;
+  return 0;
+}
