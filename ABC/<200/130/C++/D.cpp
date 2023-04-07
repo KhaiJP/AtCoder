99895d7eb473e0eb8da5d2aedb8129deca65544b
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-04-07 12:37:35 +0900

    Solved 130D

--- ABC/<200/130/C++/D.cpp
+++ ABC/<200/130/C++/D.cpp
@@ -1,39 +1,23 @@
 #include<iostream>
 using namespace std;
 using ll = int64_t;
-#define rep(i, s, t) for(int i = (s); i < (t); ++i)
+#define rep(i, s, t) for(ll i = (s); i < (t); ++i)
 
-ll A[100010];
+ll A[100010], pfx[100010];
 
 int main(){
   ll N, K; cin >> N >> K;
-  rep(i, 0, N) cin >> A[i];
-
+  rep(i, 1, N+1) cin >> A[i];
+  rep(i, 1, N+1) pfx[i] = pfx[i-1] + A[i];
   // answer = (total number of subsequences) - (number of subsequences whose sum < K)
   // the total number = N + N-1 + ... + 1 = N * (N - 1) / 2
   ll tot = N * (N + 1) / 2;
 
   // two pointers
-  ll l = 0, r = 0, numLessK = 0, tmpSum = 0;
-  while(true){
-    if(r == N){
-      ++l;
-      ll M = (r-1) - l + 1;
-      numLessK += M * (M + 1) / 2;
-      break;
-    }
-    
-    if(tmpSum + A[r] < K){
-      tmpSum += A[r];
-      ++numLessK;
-      ++r;
-    }else if(r == l){
-      ++r, ++l;
-    }else{
-      tmpSum -= A[l];
-      ++l;
-      numLessK += (r-1) - l + 1;
-    }
+  ll r = 1, numLessK = 0;
+  rep(l, 1, N+1){
+    while(r <= N && pfx[r] - pfx[l-1] < K) ++r;
+    numLessK += r - l;
   }
 
   cout << tot - numLessK << endl;
