65e16456c3c8dcb41d137ef60f0b5715b792265d
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-04-08 16:57:45 +0900

    Solved 295D

--- ABC/<300/295/C++/D.cpp
+++ ABC/<300/295/C++/D.cpp
@@ -2,12 +2,13 @@
 #include<string>
 #include<map>
 using namespace std;
+using ll = int64_t;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
 
 int main(){
   string S; cin >> S;
 
-  map<int, int> m;
+  map<int, ll> m;
   int ref = 0;
   ++m[ref];
   for(char c : S){
@@ -16,9 +17,9 @@ int main(){
     ++m[ref];
   }
 
-  int answer = 0;
+  ll answer = 0;
   for(auto p : m){
-    int &a = p.second;
+    ll &a = p.second;
     answer += a * (a - 1) / 2;
   }
 
