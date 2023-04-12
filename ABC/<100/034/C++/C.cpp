e97a489600ee450494590fe1bcce9fbb73a338e2
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-04-13 03:27:57 +0900

    Solved 034C

--- ABC/<100/034/C++/C.cpp
+++ ABC/<100/034/C++/C.cpp
@@ -1,22 +1,35 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
+using ll = int64_t;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
 
-int main(){
-  string S; cin >> S; S += '+';
-  int answer = 0;
-  bool is0there = false;
-  rep(i, 0, S.size()){
-    if(i&1){
-      if(S[i] == '+'){
-        answer += !is0there;
-        is0there = false;
-      }
+// ref https://drken1215.hatenablog.com/entry/2018/06/08/210000
+const int MAX = 510000;
+const int MOD = 1000000007;
+ll fac[MAX], finv[MAX], inv[MAX];
+// preprocessing
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    rep(i, 2, MAX){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
     }
-    else is0there = is0there || S[i] == '0';
-  }
+}
+// combination
+ll nCk(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
 
-  cout << answer << endl;
-  return 0;
+int main() {
+  	ll W, H; cin >> W >> H;
+    // 前処理
+    COMinit();
+
+    // 計算例
+    cout << nCk(W+H-2, W-1) << endl;
 }
