f747c0113c0ff9dc5696bb1e7fc6040cce51b4cd
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-04-16 00:20:43 +0900

    Solved 298A

---
+++ ABC/<300/298/C++/A.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<deque>
+#include<queue>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = int64_t;
+using Edges = unordered_map<ll, vector<ll>>;
+using Weights = map<pair<ll, ll>, ll>;
+#define rep(i, s, t) for(int i = (s); i < (t); ++i)
+#define rrep(i, s, t) for(int i = (s); i >= (t); --i)
+#define repc(c) for(char c = 'a'; c != 'z'+1; ++c)
+#define fore(a, v) for(auto a : v)
+#define yes "Yes"
+#define noo "No"
+
+const int inf32 =  1000000000+7;
+const int64_t inf64 =  1000000000000000000;
+
+int main(){
+
+
+  return 0;
+}
