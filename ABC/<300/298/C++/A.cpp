1ffa22c2d31f4b9b04f5a5cf23c7eb82a04c22a4
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-04-16 00:20:58 +0900

    Solved 298A

--- ABC/<300/298/C++/A.cpp
+++ ABC/<300/298/C++/A.cpp
@@ -1,34 +1,19 @@
 #include<iostream>
-#include<iomanip>
-#include<vector>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<unordered_set>
-#include<deque>
-#include<queue>
-#include<string>
-#include<algorithm>
-#include<numeric>
-#include<utility>
-#include<atcoder/all>
 using namespace std;
-using namespace atcoder;
-using ll = int64_t;
-using Edges = unordered_map<ll, vector<ll>>;
-using Weights = map<pair<ll, ll>, ll>;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
-#define rrep(i, s, t) for(int i = (s); i >= (t); --i)
-#define repc(c) for(char c = 'a'; c != 'z'+1; ++c)
-#define fore(a, v) for(auto a : v)
-#define yes "Yes"
-#define noo "No"
-
-const int inf32 =  1000000000+7;
-const int64_t inf64 =  1000000000000000000;
 
 int main(){
 
+  int N; cin >> N;
+  int maru = 0, batu = 0;
+  rep(i, 0, N){
+    string S; cin >> S;
+    for(auto c : S){
+      maru += c == 'o';
+      batu += c == 'x';
+    }
+  }
 
+  cout << (maru > 0 && batu == 0 ? "Yes" : "No")<< endl;
   return 0;
 }
