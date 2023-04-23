aea5287d63d74bb8c0cf1df678269e81a18ad402
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-04-23 22:16:12 +0900

    Update B.cpp

--- ABC/<300/299/C++/B.cpp
+++ ABC/<300/299/C++/B.cpp
@@ -1,37 +1,25 @@
 #include<iostream>
 #include<map>
-#include<set>
+#include<algorithm>
+#include<vector>
 using namespace std;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
 
-pair<int, int> CR[200010], Ri[200010];
-map<int, set< pair< int, int > > > C_Ri;
+int C[200010];
+map< int, vector<pair<int, int>> > C_Ri;
 
 int main(){
- int N, T; cin >> N >> T;
+  int N, T; cin >> N >> T; 
 
- rep(i, 0, N) cin >> CR[i].first;
- rep(i,0,N){
-  cin >> CR[i].second;
-  Ri[i] = {-CR[i].second, i+1};
- }
-
- rep(i, 0, N){
-  int &C = CR[i].first, &R = CR[i].second;
-  C_Ri[C].insert({-R, i+1});
- }
-
-  if(C_Ri.count(T) == 1){
-    auto &p = *C_Ri[T].begin();
-    cout << p.second << endl;
-    return 0;
+  rep(i, 0, N) cin >> C[i];
+  int &C0 = C[0];
+  rep(i, 0, N){
+    int R; cin >> R;
+    if(C[i] == T || C[i] == C0) C_Ri[C[i]].push_back({-R, i+1});
   }
+  for(auto &p : C_Ri) sort(p.second.begin(), p.second.end());
 
-  else{
-    int &C1 = CR[0].first;
-    auto &p = *C_Ri[C1].begin();
-    cout << p.second << endl;
-    return 0;
-  }
+  int key = C_Ri.count(T) == 1 ? T : C0; 
+  cout << C_Ri[key][0].second << endl;
   return 0;
 }
