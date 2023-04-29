d283b656526d06476df5f6f61f9d95bca4a8ff78
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-04-30 00:22:29 +0900

    Solved 300A

---
+++ ABC/<400/300/C++/A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#define rep(i, s, t) for(int i = (s); i < (t); ++i)
+
+int main(){
+  int N, A, B; cin >> N >> A >> B;
+  map<int, int> m;
+
+  rep(i, 1, N+1){
+    int C; cin >> C;
+    if(A+B == C){
+      cout << i << endl;
+      return 0;
+    }
+  }
+}
