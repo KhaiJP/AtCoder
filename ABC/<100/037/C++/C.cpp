95782d2d608d6042b463d6fd485e77ec09041678
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-06-01 19:54:54 +0900

    Solved 037

---
+++ ABC/<100/037/C++/C.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+using ll = int64_t;
+#define rep(i, s, t) for(int i = (s); i < (t); ++i)
+
+ll pfx[100010];
+
+int main(){
+  int N, K; cin >> N >> K;
+  rep(i, 0, N){
+    int a; cin >> a;
+    pfx[i+1] = pfx[i] + a;
+  }
+
+  ll answer = 0;
+  rep(i, K, N+1) answer += pfx[i] - pfx[i-K];
+
+  cout << answer << endl;
+  return 0;
+}
