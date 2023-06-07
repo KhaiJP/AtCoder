20de4833a54797846f0db7e4f0cc4dff058ff8ad
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-06-07 19:22:35 +0900

    Update C.cpp

--- ABC/<100/077/C++/C.cpp
+++ ABC/<100/077/C++/C.cpp
@@ -5,6 +5,7 @@ using ll = int64_t;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
 #define rrep(i, s, t) for(int i = (s); i >= (t); --i)
 
+// pfx[i]; sum( #({ k | C[k] > B[l] }) | l >= i ) here B and C are sorted in increasing order
 ll A[100010], B[100010], C[100010], pfx[100010];
 
 int main(){
@@ -14,12 +15,13 @@ int main(){
   rep(i, 0, N) cin >> C[i];
   sort(A, A + N), sort(B, B + N), sort(C, C + N);
 
-  auto jc = upper_bound(C, C + N, B[N-1]) - C;
-  pfx[N-1] = N - jc;
+  // j = min( { k | C[k] > B[N-1] } ) -> #( { k | C[k] > B[N-1] } ) = N - j
+  auto j = upper_bound(C, C + N, B[N-1]) - C;
+  pfx[N-1] = N - j;
 
   rrep(i, N-2, 0){
-    auto ic = upper_bound(C, C + N, B[i]) - C;
-    pfx[i] = pfx[i + 1] + (N - ic); 
+    auto j = upper_bound(C, C + N, B[i]) - C;
+    pfx[i] = pfx[i + 1] + (N - j); 
   }
 
   ll answer = 0;
