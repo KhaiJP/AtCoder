6e00a5c18d4f485a2a9e50957c726ef101680ff7
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-06-25 12:42:25 +0900

    Update C.cpp

--- ABC/<400/306/C++/C.cpp
+++ ABC/<400/306/C++/C.cpp
@@ -1,26 +1,16 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
 
-vector<int>    FA[100010];
-pair<int, int> V[100010];
+int A[100010];
 
 int main(){
   int N; cin >> N;
   rep(i, 0, 3*N){
-    int a; cin >> a; --a;
-    FA[a].push_back(i+1);
+    int a; cin >> a;
+    ++A[a];
+    if(A[a] == 2) cout << a << ' ';
   }
-
-  rep(i, 0, N){
-    sort(FA[i].begin(), FA[i].end());
-    V[i] = {FA[i][1], i+1};
-  }sort(V, V+N);
-
-  rep(i, 0, N) cout << V[i].second << ' ';
   cout << endl;
-
   return 0;
 }
