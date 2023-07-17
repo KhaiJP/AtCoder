e4bd7387122e01e3d884de079d238fd6b00ba26e
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-07-17 09:05:28 +0900

    Solved 136D

---
+++ ABC/<200/136/C++/D.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<deque>
+#include<queue>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = int64_t;
+using Edges = unordered_map<ll, vector<ll>>;
+using Weights = map<pair<ll, ll>, ll>;
+#define rep(i, s, t) for(int i = (s); i < (t); ++i)
+#define rrep(i, s, t) for(int i = (s); i >= (t); --i)
+#define repc(c) for(char c = 'a'; c != 'z'+1; ++c)
+#define fore(a, v) for(auto a : v)
+#define yes "Yes"
+#define noo "No"
+
+const int inf =  1000000000+7;
+const ll llinf =  1000000000000000000;
+const ll ll1   = 1;
+int DR[] = {-1,  0,  1,  0, -1,  1,  1, -1};
+int DC[] = { 0,  1,  0, -1,  1,  1, -1, -1};
+
+int main(){
+
+
+  return 0;
+}
