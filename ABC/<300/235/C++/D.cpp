64bada42a466b4622839bc4e500496f824856429
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-08-05 18:33:59 +0900

    Update D.cpp

--- ABC/<300/235/C++/D.cpp
+++ ABC/<300/235/C++/D.cpp
@@ -1,66 +1,51 @@
 #include<iostream>
 #include<string>
-#include<vector>
-#include<deque>
+#include<queue>
 using namespace std;
-typedef int64_t ll;
-ll ston(string s);
-string ntos(ll n);
-void rotate(string &s);
+using ll = int64_t;
+#define rep(i, s, t) for(int i = (s); i < (t); ++i)
+
+ll a, N;
+int dist[10000010];
+
+
+ll rotate(ll x){
+  string xs = to_string(x);
+  string s = xs.back() + xs; s.erase(s.size()-1);
+  return stoi(s);
+}
 
 int main(){
-  ll a, N; cin >> a >> N;
-  
+  cin >> a >> N;
   ll M = 1;
-  while(M <= N) M *= 10;
+  while (M <= N) M *= 10;
+  
+  queue<ll> q; q.push(1);
+  while(q.size()){
+    ll n = q.front(); q.pop();
+    ll d = dist[n];
 
-  vector<ll> num_operations(M, -1);
-  deque<ll> dq;
-  num_operations[1] = 0;
-  dq.push_back(1);
-
-  while(!dq.empty()){
-    ll n = dq.front(); dq.pop_front();
-    ll &nos = num_operations[n];
-
-    ll op1 = a*n;
-    if(op1 < M && num_operations[op1] == -1){
-      num_operations[op1] = nos+1;
-      dq.push_back(op1);
+    ll an = a * n;
+    if(an < M && dist[an] == 0 ){
+      dist[an] = d + 1;
+      q.push(an);
     }
 
-    if(n >= 10 && n%10 != 0){
-      string s = ntos(n); rotate(s);
-      ll op2 = ston(s);
-      if(op2 < M && num_operations[op2] == -1){
-        num_operations[op2] = nos+1;
-        dq.push_back(op2);
+    ll rn = -1;
+    if(n > 10 && n % 10 != 0){
+      rn = rotate(n);
+      if(rn < M && dist[rn] == 0){
+        dist[rn] = d + 1;
+        q.push(rn);
       }
     }
-  }
-  cout << num_operations[N] << endl;
-  return 0;
-}
 
-ll ston(string s){
-  ll ret = 0;
-  ll ten = 1;
-  for(ll i = s.size()-1; i >= 0; --i){
-    ret += ten*((ll)(s[i]-'0'));
-    ten *= 10;
+    if(an == N || rn == N){
+      cout << d + 1 << endl;
+      return 0;
+    }
   }
-  return ret;
-}
-string ntos(ll n){
-  if(n == 0) return "0";
-  string ret = "";
-  while(n > 0){
-    ret = (char)(n%10 + '0') + ret;
-    n /= 10;
-  }
-  return ret;
-}
-void rotate(string &s){
-  s = s[s.size()-1]+s;
-  s.erase(s.size()-1);
+
+  cout << -1 << endl;
+  return 0;
 }
