f8cb93923a5edf730b139334d5d415e707385c20
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-08-10 20:10:07 +0900

    Solved 147D

--- ABC/<200/147/C++/D.cpp
+++ ABC/<200/147/C++/D.cpp
@@ -1,25 +1,36 @@
 #include<iostream>
+#include<atcoder/all>
 using namespace std;
 using ll = int64_t;
+using mint = atcoder::modint1000000007;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
 
-ll dp[1010][1010];
+// this problem is almost equivalent to the following problem;
+// there are N integers Ai. each number is either 0 or 1.
+// find \sum_{i,j; i!=j} Ai^Aj.
+// Since 0^0 = 1^1 = 0, the answer is the number of unordered pairs {i,j} s.t. {Ai, Aj} = {0, 1}
+// this number can be calculated by (# of 0s) * (# of 1s)
+// the original problem can be solved by repeating the algo above 60 times
+ll A[300010];
 
 int main(){
   int N; cin >> N;
+  rep(i, 0, N) cin >> A[i];
   
-  int t = 1, a = 1;
-  dp[t][a] = 2;
+  mint answer = 0;
+  ll b = 1;
+  rep(i, 0, 60){
+    // # of 0s and # of 1s
+    int zeros = 0, ones = 0;
+    rep(j, 0, N){
+      if(A[j] & b) ++ones;
+      else         ++zeros;
+    }
 
-  rep(i, 0, N){
-    int T, A; cin >> T >> A;
-    ll q  = dp[t][a] / (t + a);
-    ll vT = q * t, vA = q * a;
-    ll n  = max((vT + T -1) / T, (vA + A - 1) / A);
-    dp[T][A] = n * (T + A);
-    t = T, a = A;  
+    answer += (mint)b * zeros * ones;
+    b <<= 1;
   }
-  
-  cout << dp[t][a] << endl;
+
+  cout << answer.val() << endl;
   return 0;
 }
