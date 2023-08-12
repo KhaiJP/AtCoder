68cac5c147861c22ebd6a34587ccad2bddadf7b2
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-08-13 07:52:30 +0900

    Update D.cpp

--- ABC/<400/312/C++/D.cpp
+++ ABC/<400/312/C++/D.cpp
@@ -1,32 +1,32 @@
 #include<iostream>
+#include<string>
 #include<atcoder/all>
 using namespace std;
-using namespace atcoder;
-using mint = modint998244353;
+using mint = atcoder::modint998244353;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
 
 mint dp[3010][3010];
 
 int main(){
-  string s; cin >> s;
-  int N = s.size();
+  string S; cin >> S;
+  int N = S.size();
+  
   dp[0][0] = 1;
-
-  rep(i, 0, N)rep(j, 0, N+1){
-    if(s[i] == '('){
-      dp[i+1][j+1] += dp[i][j]; 
+  rep(i, 0, N)rep(j, 0, N/2+2)if(dp[i][j].val() != 0){
+    if(S[i] == '?'){
+      if(j) dp[i+1][j-1] += dp[i][j];
+      dp[i+1][j+1] += dp[i][j];
     }
-
-    if(s[i] == ')'){
-      if(j-1 >= 0) dp[i+1][j-1] += dp[i][j]; 
+    
+    if(S[i] == ')')if(j){
+      dp[i+1][j-1] += dp[i][j];
     }
-
-    if(s[i] == '?'){
-      dp[i+1][j+1] += dp[i][j]; 
-      if(j-1 >= 0) dp[i+1][j-1] += dp[i][j]; 
+    
+    if(S[i] == '('){
+      dp[i+1][j+1] += dp[i][j];
     }
   }
-
+  
   cout << dp[N][0].val() << endl;
   return 0;
 }
