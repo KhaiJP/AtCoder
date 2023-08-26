e97e0f4f56a81fb38c8d45a694c9e5133504c66a
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-08-26 23:50:37 +0900

    Solved 317A

---
+++ ABC/<400/317/C++/A.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int N, H, X; cin >> N >> H >> X;
+  for(int i = 1; i <= N; ++i){
+    int P; cin >> P;
+    if(H + P >= X){
+      cout << i << endl;
+      return 0;
+    }
+  }
+}
