2934702f672db63a9152b9b97d0c795329607aa4
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-08-30 08:10:44 +0900

    Update D.cpp

--- ABC/<300/263/C++/D.cpp
+++ ABC/<300/263/C++/D.cpp
@@ -1,23 +1,32 @@
 #include<iostream>
-#include<vector>
-typedef int64_t ll;
-#define rep(i, s, t) for (ll i = (s); i < (t); ++i)
-#define ren(i, s, t) for (ll i = (s); i >= (t); --i)
 using namespace std;
+using ll = int64_t;
+#define rep(i, s, t) for(ll i = (s); i < (t); ++i)
+#define chmin(a, b) a = min(a, b);
+
+ll N, L, R;
+ll A[200010], dp[200010][3];
+ll inf = 2e18;
 
 int main(){
-  int N, L, R; cin >> N >> L >> R;
-  vector<ll>  A(N+1); rep(i,1,N+1) cin >> A[i];
-  vector DP(N+1, vector<ll>(3, 1000000000000000000));
-  DP[0][0] = 0;
+  cin >> N >> L >> R;
+  rep(i, 0, N) cin >> A[i];
+
+  rep(i, 0, 200010)rep(s, 0, 3) dp[i][s] = inf;
+  dp[0][0] = 0;
+
   rep(i, 0, N)rep(s, 0, 3){
-    if(DP[i][s] == 1000000000000000000) continue;
-    if( true ) DP[i+1][2] = min(DP[i+1][2], DP[i][s] + R);
-    if(s != 2) DP[i+1][1] = min(DP[i+1][1], DP[i][s] + A[i+1]);
-    if(s == 0) DP[i+1][0] = min(DP[i+1][0], DP[i][s] + L);
+    chmin(dp[i+1][2], dp[i][s] + R);
+    if(s == 2) continue;
+
+    chmin(dp[i+1][1], dp[i][s] + A[i]);
+    if(s == 1) continue;
+
+    chmin(dp[i+1][0], dp[i][s] + L);
   }
-  ll answer = 1000000000000000000;
-  rep(s,0,3) answer = min(answer, DP[N][s]);
-  cout << answer << '\n';
+
+  ll answer = inf;
+  rep(s, 0, 3) chmin(answer, dp[N][s]);
+  cout << answer << endl;
   return 0;
 }
