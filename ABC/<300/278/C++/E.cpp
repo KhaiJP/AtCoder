1f4ccce647eb7074d372293e48ce82c4851d4602
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-08-30 22:46:42 +0900

    Update E.cpp

--- ABC/<300/278/C++/E.cpp
+++ ABC/<300/278/C++/E.cpp
@@ -1,51 +1,37 @@
-// this script highly refers to https://www.youtube.com/watch?v=60CJDyHJ11Q
 #include<iostream>
-#include<vector>
-#define rep(i,n) for(int i=0; i<(n); ++i)
-
+#include<set>
 using namespace std;
+#define rep(i, s, t) for(int i = (s); i < (t); ++i)
+
+int H, W, N, h, w, A[310][310];
+int cnt[310][310][310];
+
+int solve(int r, int c){
+  int answer = 0;
+  rep(n, 1, N+1){
+    int tmp = 0;
+    tmp  = cnt[H][W][n];
+    tmp -= cnt[r+h-1][c+w-1][n];
+    tmp += cnt[r+h-1][c-1][n] + cnt[r-1][c+w-1][n];
+    tmp -= cnt[r-1][c-1][n];
+    answer += tmp > 0; 
+  }
+  return answer;
+}
 
 int main(){
-    // std input
-    int H, W, N, h, w;
-    cin >> H >> W >> N >> h >> w;
-    vector<vector<int>> A(H, vector<int>(W));
-    rep(row, H)rep(col, W) cin >> A[row][col];
+  cin >> H >> W >> N >> h >> w;
+  rep(r, 1, H+1)rep(c, 1, W+1) cin >> A[r][c];
+  rep(r, 1, H+1)rep(c, 1, W+1) ++cnt[r][c][A[r][c]];
 
-    vector<vector<int>> ans(H-h+1, vector<int>(W-w+1));
-    
-    /*
-    grow: indicates row number of A
-    lrow: indicates row number of the black rectangle
-    gcol: indicates col number of A
-    lcol: indicates col number of the black rectangle */
-    rep(grow, H-h+1){
-        // counter[num]: number of num in the grid
-        vector<int> counter(N+1, 0);
-        // number of numbers appeared in the whole grid except black area
-        int num_kinds = 0;
-        /* ----------------------------------------------*/
-        auto add_to_counter = [&](int row, int col, int change=1){
-            int x = A[row][col];
-            if(counter[x] == 0) num_kinds++;
-            counter[x] += change;
-            if(counter[x] == 0) num_kinds--;
-        };
-        auto del_from_counter = [&](int i, int j){ add_to_counter(i, j, -1);};
-        /* ----------------------------------------------*/
-        // counts the number of num in the whole grid
-        rep(row, H)rep(col, W) add_to_counter(row, col);
-        // discounts, from the whole grid, the number of num in the black grid that stays at the left-most
-        rep(lrow, h)rep(lcol, w-1) del_from_counter(grow+lrow, lcol);
-        rep(gcol, W-w+1){
-            rep(row, h) del_from_counter(grow+row, w-1+gcol);
-            ans[grow][gcol] = num_kinds;
-            rep(row, h) add_to_counter(grow+row, gcol);
-        }
-    }
-    rep(row, ans.size()){
-        rep(col, ans[row].size()) cout << ans[row][col] << " ";
-        cout << "\n";
-    }
-    return 0;
+  // 2 dimensional prefix sum
+  rep(n, 1, N+1)rep(r, 1, H+1)rep(c, 1, W+1) cnt[r][c][n] += cnt[r][c-1][n];
+  rep(n, 1, N+1)rep(c, 1, W+1)rep(r, 1, H+1) cnt[r][c][n] += cnt[r-1][c][n];
+
+  rep(r, 1, H-h+2){
+    rep(c, 1, W-w+2) cout << solve(r, c) << ' ';
+    cout << endl;
+  }
+
+  return 0;
 }
