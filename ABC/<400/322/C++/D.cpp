6bcc8a4a90b047846f6170a35ece5e3cf7dff6ed
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-10-01 09:13:47 +0900

    Update D.cpp

--- ABC/<400/322/C++/D.cpp
+++ ABC/<400/322/C++/D.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 #define rep(i, s, t) for(int i = (s); i < (t); ++i)
 
-int F[200][200], head[10];
+int F[20][20], head[10];
 set<pair<int,int>> P[3];
 
 bool isFoccupied(){
@@ -23,45 +23,41 @@ void putPolyominos(int isput){
     for(auto p : P[i]){
       int u = p.first, v = p.second;
       int r, c;
+      // rotation
       if(head[3*i + 2] == 0) r =  u, c =  v;
       if(head[3*i + 2] == 1) r = -v, c =  u;
       if(head[3*i + 2] == 2) r = -u, c = -v;
       if(head[3*i + 2] == 3) r =  v, c = -u;
-      F[head[3*i] + r][head[3*i+1] + c] += isput;
+      F[10 + head[3*i] + r][10 + head[3*i+1] + c] += isput;
     }
   }
 }
 
 int main(){
   rep(i, 0, 3){
+    // the left-most square on the top row is set to (0, 0)
+    // other squares are specified by relative positions
     int u = -1, v = -1;
     rep(r, 0, 4)rep(c, 0, 4){
       char p; cin >> p;
       if(p != '#') continue;
-      if(u == -1 && v == -1){
-        u = r, v = c;
-        P[i].insert({0, 0});
-      }else{
-        P[i].insert({r-u, c-v});
-      }
+      if(u == -1 && v == -1) u = r, v = c;
+      P[i].insert({r-u, c-v});
     }
   }
 
-  rep(r0, 10, 14)rep(c0, 10, 14)rep(rot0, 0, 4){
-    head[0] = r0, head[1] = c0, head[2] = rot0;
-    rep(r1, 10, 14)rep(c1, 10, 14)rep(rot1, 0, 4){
-      head[3] = r1, head[4] = c1, head[5] = rot1;
-      rep(r2, 10, 14)rep(c2, 10, 14)rep(rot2, 0, 4){
-        head[6] = r2, head[7] = c2, head[8] = rot2;
-        
-        putPolyominos(1);
-        if(isFoccupied()){
-          cout << "Yes" << endl;
-          return 0;
-        }
-        putPolyominos(-1);
-      }
+  rep(s0, 0, 64)rep(s1, 0, 64)rep(s2, 0, 64){
+    int t0 = s0%16, t1 = s1%16, t2 = s2%16;
+    head[0] = t0/4, head[1] = t0%4, head[2] = s0/16;
+    head[3] = t1/4, head[4] = t1%4, head[5] = s1/16;
+    head[6] = t2/4, head[7] = t2%4, head[8] = s2/16;
+
+    putPolyominos(1);
+    if(isFoccupied()){
+      cout << "Yes" << endl;
+      return 0;
     }
+    putPolyominos(-1);
   }
   
   cout << "No" << endl;
