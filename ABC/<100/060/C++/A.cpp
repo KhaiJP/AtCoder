cddab5eccd8ad609d2ba0a012f67c7f5162e202a
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-12-11 20:55:21 +0900

    Solved 060A

---
+++ ABC/<100/060/C++/A.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  string A, B, C; cin >> A >> B >> C;
+  if(A.back() == B[0] && B.back() == C[0]){
+    cout << "YES" << endl;
+  }else{
+    cout << "NO" << endl;
+  }
+  return 0;
+}
