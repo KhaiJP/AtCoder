449941624b058ea9bb2fde8fc158be890ca73d7f
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2023-12-16 23:39:53 +0900

    Solved 333B

---
+++ ABC/<400/333/C++/B.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+bool is_adjacent(char a, char b){
+  if(a > b) swap(a, b);
+  return (b-a == 1) || (a == 'A' && b == 'E');
+}
+
+int main(){
+  string S, T; cin >> S >> T;
+  bool fs = is_adjacent(S[0], S[1]);
+  bool ft = is_adjacent(T[0], T[1]);
+  cout << (fs == ft ? "Yes" : "No") << endl;
+  return 0;
+}
