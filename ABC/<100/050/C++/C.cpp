8bbd70b393b1cf326c0a85feefa4082cb71c65fd
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2024-02-28 07:27:54 +0900

    Update C.cpp

--- ABC/<100/050/C++/C.cpp
+++ ABC/<100/050/C++/C.cpp
@@ -1,24 +1,31 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-#define rep(i,s,t) for(int i = (s); i < (t); ++i)
-const int MOD = 1000000000+7;
+using ll = int64_t;
+#define rep(i, s, t) for(ll i = (s); i < (t); ++i)
+
+// if N is odd, A must be one of the perputations of 0, 2, 2, 4, 4, ... , N-1, N-1.
+// if even, 1, 1, 3, 3, 5, 5, ..., N-1, N-1.
+// the actual order must be N-1, ... , 2, 0, 2, ... , N-1 (odd) or N-1, ... , 1, 1, ... , N-1 (even)
+// hence the answer is 2^(N/2), or 0 iff A is not one of the permutations.
+
+ll N, cnt[100010], answer=1, mod=1e9+7;
 
 int main(){
-  int N; cin >> N;
-  vector<int> P(N+1, 0);
-  
-  int mid = N/2 + N%2;
-  rep(i,1,N+1){
+  cin >> N;
+  rep(i, 0, N){
     int A; cin >> A;
-    ++P[mid - A/2];
+    ++cnt[A];
   }
 
-  int answer = 1;
-  rep(i,1,mid) answer = (answer*(P[i]==2 ? 2 : 0))%MOD;
-  if(N%2 == 1 && P[mid] != 1) answer *= 0;
-  if(N%2 == 0) answer = (answer*2)%MOD;
-  cout << answer << endl;
+  // if N is odd, only 0 emerges once
+  bool f = (N&1 ? (cnt[0] == 1) : true);
+  rep(i, 1, N)if((i&1) != (N&1)){
+    // check if A is one of the permutations
+    f = f && (cnt[i] == 2);
+    // calculating 2^(N/2) mod 1e9+7
+    answer = answer * 2 % mod;
+  }
 
+  cout << (f ? answer : 0) << endl;
   return 0;
 }
