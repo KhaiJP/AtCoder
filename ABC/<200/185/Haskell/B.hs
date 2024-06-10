a16b78e8458ff07f4b945263dd9d7d300b6b9c7e
Khai <storage27592@gmail.com>
2024-06-10 09:54:03 +0900

    Solved 185 in Haskell

---
+++ ABC/<200/185/Haskell/B.hs
@@ -0,0 +1,25 @@
+import Control.Monad
+import Data.Maybe ( isJust )
+
+
+main = do
+    [n, m, t] <- getInts
+    abS' <- replicateM m $ (\[a, b] -> (a, b)) <$> getInts
+    let abS = reverse $ abS' ++ [(t, t)]
+    let goodBattery = goodBattery' n
+    let result = foldr (\inOut mState -> mState >>= goodBattery inOut) (return (n, 0)) abS
+    putStrLn $ if isJust result then "Yes" else "No"
+
+
+goodBattery' :: Int -> (Int, Int) -> (Int, Int) -> Maybe (Int, Int)
+goodBattery' maxVolume (battery, now) (shopIn, shopOut)
+    | battery' <= 0 = Nothing
+    | otherwise     = Just (battery'', shopOut)
+    where
+        battery'  = battery - (shopIn - now)
+        battery'' = min maxVolume $ battery' + (shopOut - shopIn)
+
+
+getInts :: IO [Int]
+getInts = map read . words <$> getLine
+
